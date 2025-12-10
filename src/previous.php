5691a9bf80521dd472a73de8a34dfd5b405d409e
Ulyana1104 <145419281+UlyanaSmirnova11@users.noreply.github.com>
2025-12-10 21:47:52 +0500

    Update previous.php

--- src/previous.php
+++ src/previous.php
@@ -10,11 +10,19 @@ try {
     
     echo "<h1>История тестов</h1>";
     
+    // Русские названия темпераментов
+    $russian_names = [
+        'choleric' => 'Холерик',
+        'sanguine' => 'Сангвиник', 
+        'phlegmatic' => 'Флегматик',
+        'melancholic' => 'Меланхолик'
+    ];
+    
     if (empty($results)) {
         echo "<p>Пока нет пройденных тестов.</p>";
     } else {
-        echo "<table border='1' cellpadding='10'>";
-        echo "<tr>
+        echo "<table border='1' cellpadding='10' style='border-collapse: collapse;'>";
+        echo "<tr style='background-color: #f2f2f2;'>
                 <th>Имя</th>
                 <th>Холерик</th>
                 <th>Сангвиник</th>
@@ -27,11 +35,11 @@ try {
         foreach ($results as $result) {
             echo "<tr>";
             echo "<td>" . htmlspecialchars($result['user_name']) . "</td>";
-            echo "<td>" . $result['choleric_score'] . "</td>";
-            echo "<td>" . $result['sanguine_score'] . "</td>";
-            echo "<td>" . $result['phlegmatic_score'] . "</td>";
-            echo "<td>" . $result['melancholic_score'] . "</td>";
-            echo "<td>" . ucfirst($result['dominant_temperament']) . "</td>";
+            echo "<td align='center'>" . $result['choleric_score'] . "</td>";
+            echo "<td align='center'>" . $result['sanguine_score'] . "</td>";
+            echo "<td align='center'>" . $result['phlegmatic_score'] . "</td>";
+            echo "<td align='center'>" . $result['melancholic_score'] . "</td>";
+            echo "<td align='center'>" . $russian_names[$result['dominant_temperament']] . "</td>";
             echo "<td>" . $result['test_date'] . "</td>";
             echo "</tr>";
         }
@@ -39,6 +47,7 @@ try {
         echo "</table>";
     }
     
+    echo "<br>";
     echo "<p><a href='test.php'>Пройти тест</a></p>";
     echo "<p><a href='index.php'>На главную</a></p>";
     
