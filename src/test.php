6fe68196ce3fc9b89d3088db4b0dffc7cf448e3a
Ulyana1104 <145419281+UlyanaSmirnova11@users.noreply.github.com>
2025-12-10 23:13:40 +0500

    Update test.php

--- src/test.php
+++ src/test.php
@@ -8,29 +8,16 @@ try {
     echo "<h1>Тест на темперамент</h1>";
     echo "<form method='POST' action='results.php'>";
     echo "<label>Ваше имя: <input type='text' name='name' required></label><br><br>";
+        
+       foreach ($questions as $i => $q) {
+    echo "<p><strong>" . ($i+1) . ". " . $q['question_text'] . "</strong></p>";
     
-    foreach ($questions as $i => $q) {
-        echo "<p><strong>" . ($i+1) . ". " . $q['question_text'] . "</strong></p>";
-        
-        $options = [$q['option_a'], $q['option_b'], $q['option_c'], $q['option_d']];
-        $hasOptions = false;
-        
-        foreach ($options as $index => $option) {
-            if (!empty($option)) {
-                $hasOptions = true;
-                echo ($index+1) . " <input type='radio' name='q" . $q['id'] . "' value='" . ($index+1) . "' required> " . htmlspecialchars($option) . " ";
-            }
-        }
-        
-        // Если нет вариантов, показываем шкалу 1-5
-        if (!$hasOptions) {
-            for ($j = 1; $j <= 5; $j++) {
-                echo $j . " <input type='radio' name='q" . $q['id'] . "' value='" . $j . "' required> ";
-            }
-        }
-        
-        echo "<br><br>";
+    for ($j = 1; $j <= 5; $j++) {
+        echo $j . " <input type='radio' name='q" . $q['id'] . "' value='" . $j . "' required> ";
     }
+    
+    echo "<br><br>";
+}
     
     echo "<button type='submit'>Узнать результат</button>";
     echo "</form>";
